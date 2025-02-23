f170ec51ec497e395b2164d515baee4240cf79a9
OndrejZapletal99 <143550046+OndrejZapletal99@users.noreply.github.com>
2025-02-23 13:15:27 +0100

    web srcaping class

---
+++ module_folder/classes.py
@@ -0,0 +1,34 @@
+import bs4
+import requests
+import pandas as pd
+import datetime as dt
+
+
+class WebScraper:
+
+    def __init__(self,url):
+
+        self.url = url
+        self.res = None
+        self.soap = None
+        self.articles = set()
+        self.download_date = dt.datetime.now().strftime('%Y-%M-%d')
+
+    def web_reader(self):
+
+        self.res = requests.get(self.url)
+        self.soap = bs4.BeautifulSoup(self.res.text, 'lxml')
+    
+    def article_find(self):
+
+        for article in self.soap.select('.c_aM.c_aP'):
+            title = article.get_text(strip = True)
+            a_tag = article.find('a')
+            if a_tag and 'href' in a_tag.attrs and title != '':
+                link = a_tag['href']
+                self.articles.add((title, link, self.download_date))
+
+    def articles_to_df(self):
+        
+        df_articles = pd.DataFrame(self.articles, columns=['Title', 'Link', 'Download_Date'])
+        return df_articles
