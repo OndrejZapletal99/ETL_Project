04e9aa782a8c06950f68035c9f977de7befae536
OndrejZapletal99 <143550046+OndrejZapletal99@users.noreply.github.com>
2025-02-23 15:20:04 +0100

    key_words, person, main(webscraping, new columns based on key_words catroeis and person)

---
+++ module_folder/key_words.py
@@ -0,0 +1,13 @@
+key_words = {
+    'Volby': ['Volby'],
+    'ANO': ['ANO', 'Hnutí ANO'],
+    'Piráti':['Piráti','Pirát'],
+    'Spolu':['Spolu','ODS','TOP09'],
+    'Stačilo':['Stačilo','Komunisté','Komunisti'],
+    'SPD': ['SPD'],
+    'Piráti':['Pirátí','Pirát'],
+    'STAN':['STAN', 'Starostové'],
+    'Motoristé':['Motoristé'],
+}
+
+person = ['Babiš','Fiala','Havlíček','Pekarová','Turek','Konečná','Macinka','Rakušan','Jurečka']
