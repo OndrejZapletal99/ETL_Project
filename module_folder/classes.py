08ac5195255794fde70976e16003b63ed223b2d9
OndrejZapletal99 <143550046+OndrejZapletal99@users.noreply.github.com>
2025-02-24 19:50:58 +0100

    classes - SQL Manager (first phase.. not finished)

--- module_folder/classes.py
+++ module_folder/classes.py
@@ -2,6 +2,7 @@ import bs4
 import requests
 import pandas as pd
 import datetime as dt
+from sqlalchemy import create_engine, text
 
 
 class WebScraper:
@@ -32,3 +33,96 @@ class WebScraper:
         
         df_articles = pd.DataFrame(self.articles, columns=['Title', 'Link', 'Download_Date'])
         return df_articles
+
+class SqlManager:
+
+    def __init__(self,server: str ,database: str ,odbc_driver: str):
+        self.server = server
+        self.database = database
+        self.odbc_driver = odbc_driver
+        self.engine = None
+        self.connection_string = f"mssql+pyodbc://@{self.server}/{self.database}?trusted_connection=yes&driver={self.odbc_driver}"
+
+    def connect(self):
+        
+        try:
+            self.engine = create_engine(self.connection_string)
+            print('Successfully connected')
+            return self.engine
+        except Exception as e:
+            print(f'Something went wrong: {e}')
+            raise
+
+    def disconnect(self):
+        if self.engine is not None:
+            self.engine.dispose()
+            self.engine = None
+            print('Connection closed')
+
+    def execute_query_to_df(self,after_select_text: str):
+        
+        if self.engine is None:
+            print('No connection to database, please create some connection')
+
+        sql_statement = f'Select {after_select_text}'
+        try:
+            df = pd.read_sql(sql_statement, con = self.engine)
+            return df
+
+        except Exception as e:
+            print(f'Something went wrong: {e}')
+            raise
+
+    def execute_query(self,sql_statement: str):
+
+        if self.engine is None:
+            print('No connection to database, please create some connection')
+
+        try:
+
+            with self.engine.connect() as connection:
+                connection.execute(text(sql_statement))
+                connection.commit()
+
+        except Exception as e:
+            print(f"Something went wrong while executing the query: {e}")
+            raise
+
+
+    
+    def create_table(self,table_name: str, database: str ,columns_and_types: dict):
+        
+        if self.engine is None:
+            print('No connection to database, please create some connection')
+
+        try:
+
+            temp_df = self.execute_query_to_df(f"TABLE_NAME FROM [{database}].INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{table_name}'")
+            if not temp_df.empty:
+                print('Table already exists, please select another method (Append,Overwrite,Truncate)')
+            else:
+
+                print('Table does not exist and it will be created....')
+                columns_definiton = ", ".join([f'{col} {data_type}' for col, data_type in columns_and_types.items()])
+
+                create_table_query = f"CREATE TABLE [{database}].[dbo].[{table_name}] ({columns_definiton})"
+
+                self.execute_query(create_table_query)
+
+
+                check_table_query = f" * FROM [{database}].INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{table_name}'"
+                temp_df = self.execute_query_to_df(check_table_query)
+                if not temp_df.empty:
+                    print(f"Table '{table_name}' created successfully in [{database}]")
+                else:
+                    print(f"Failed to create table '{table_name}'")
+
+        except Exception as e:
+            print(f'Something went wrong {e}')
+
+
+
+
+        
+        
+        
